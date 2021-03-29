2f60c494ae47d25d429252051538d2d83e71f4c1
claudiahalip <claudia4ro@yahoo.com>
2021-03-29 11:04:11 -0500

    User model

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ApplicationController
+     def index
+        @items = Item.all
+     end
+
+     def show
+        @item = Item.find(params[:id])
+     end
+
+     def new
+     end
+
+     def create
+     end
+     
+     def update
+     end
+
+     def destroy
+     end
+end
