601a96134547b9911842f2c807ee201249e3b190
claudiahalip <claudia4ro@yahoo.com>
2021-04-08 19:26:31 -0500

    user conroller create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersContoller < ApplicationController
+    def show
+      @user = User.find(params[:id])
+    end
 
     def new
         @user = User.new
@@ -7,10 +10,17 @@ class UsersContoller < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to items_path
         else
             redirect_to signup_path
         end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 
     
