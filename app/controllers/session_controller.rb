b90e127bc778fcc7070261b12c50790f8dd299c0
claudiahalip <claudia4ro@yahoo.com>
2021-04-09 18:30:10 -0500

    destroy method in session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,11 +4,21 @@ class SessionController < ApplicationController
    end
 
    def create
-    @user = User.new
+    user = User.new
+    if user.save && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to items_path
+    else
+      flash[:message]= "There is no account to match this info.Sign up first"
+      redirect_to "/login"
+    end
 
    end
 
    def destroy
+     session.destroy[:user_id]
+     flash[:message]="You have successfully logged out."
+     redirect_to items_path
    end
 
 
